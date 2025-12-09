b6115e79d85d06ddf59400d636f9328174ea7d77
lucius1992 <142500538+lucius1992@users.noreply.github.com>
2025-12-09 19:15:48 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [1000;1000],
+}
+
+const sketch= () => {
+return({context, width,height}= =>{
+  context.fillstyle = 'white';
+  context.fillRect(0,0,width, height);
+  };
+};
+
+canvasSketch(sketch,settubgs);
